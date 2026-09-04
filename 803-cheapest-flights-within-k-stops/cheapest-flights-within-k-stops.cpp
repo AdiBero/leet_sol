12fd2d3dbc4523f28f21dd0c0273d9fc7c2415c1
AdiBero <adigamer635m@gmail.com>
2026-09-04 22:55:43 +0530

    Time: 11 ms (16.34%) | Memory: 20.2 MB (10.13%) - LeetSync

---
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        
+        vector<vector<pair<int,int>>> flight(n);
+        int ans = INT_MAX;
+        vector<vector<int>> dstt(k + 2, vector<int>(n, 1e9));
+
+
+        for( auto it : flights){
+           flight[it[0]].emplace_back(it[1],it[2]);
+
+        }
+         priority_queue<
+            tuple<int,int,int>,
+            vector<tuple<int,int,int>>,
+            greater<tuple<int,int,int>>
+        > pq;
+
+        pq.emplace(0,src,0);
+        
+
+        while(!pq.empty()){
+
+            auto temp = pq.top();
+          
+           pq.pop();
+
+            int too = get<1>(temp);
+            int price = get<0>(temp);
+            int edgeuse = get<2>(temp);
+
+            if(too == dst){
+        ans = min(ans, price);            
+}
+
+             for(auto it : flight[too]) {
+
+                    int toodst = it.first;
+                    int tooprice =  it.second;
+
+                    int totalprice = tooprice + price;
+                    int totaledges = edgeuse + 1;
+
+                    if(totaledges <= k+1){
+                        if(totalprice < dstt[totaledges][toodst]){
+                            pq.emplace(totalprice,toodst,totaledges);
+                            dstt[totaledges][toodst] = totalprice;
+                        }
+                    }
+               
+
+
+        }
+        
+    }
+   if(ans == INT_MAX){
+    return -1;
+   }
+   return ans;
+    }
+};
