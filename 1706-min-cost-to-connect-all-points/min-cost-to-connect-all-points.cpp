c4860137441b0cc092d7c3ae38574e19757f95b6
AdiBero <adigamer635m@gmail.com>
2026-09-08 18:41:34 +0530

    Time: 11 ms (99.73%) | Memory: 14.3 MB (99.87%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
@@ -3,45 +3,39 @@ public:
     int minCostConnectPoints(vector<vector<int>>& points) {
         int n = points.size();
 
-        vector<vector<pair<int,int>>> point(n);
+        vector<int> minDist(n, INT_MAX);
+        vector<bool> visited(n, false);
 
-       for(int i = 0; i < n; i++) {
-    for(int j = i + 1; j < n; j++) {
+        minDist[0] = 0;
 
-        int dist = abs(points[i][0] - points[j][0])
-                + abs(points[i][1] - points[j][1]);
-
-        point[i].push_back({j, dist});
-        point[j].push_back({i, dist});
-    }
-}
-        priority_queue<pair<int,int> , vector<pair<int,int>>, greater<pair<int,int>>> pq;
-
-        pq.emplace(0,0);
-        vector<bool> v(n,false);
         int sum = 0;
 
-        while(!pq.empty()){
-            auto temp = pq.top();
-            int weight = temp.first;
-            int node = temp.second;
+        for(int count = 0; count < n; count++) {
 
-           
-                pq.pop();
-            
-            
+            int node = -1;
 
-        
-         if(!v[temp.second]){
-            sum += weight;
-            v[node] = true;
+            for(int i = 0; i < n; i++) {
+                if(!visited[i] &&
+                   (node == -1 || minDist[i] < minDist[node])) {
+                    node = i;
+                }
+            }
 
-        for(auto it : point[node]){
-            pq.emplace(it.second,it.first);
+            visited[node] = true;
+            sum += minDist[node];
+
+            for(int j = 0; j < n; j++) {
+
+                if(!visited[j]) {
+
+                    int dist = abs(points[node][0] - points[j][0])
+                             + abs(points[node][1] - points[j][1]);
+
+                    minDist[j] = min(minDist[j], dist);
+                }
+            }
         }
-        
-    }
-        }
+
         return sum;
     }
 };
