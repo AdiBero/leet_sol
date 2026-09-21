1b1f9008e94789af414019adc4b9068e36f139f4
Aditya Singh <adigamer635m@gmail.com>
2026-09-21 19:06:09 +0530

    Time: 108 ms (40.16%) | Memory: 261.9 MB (14.11%) - LeetSync

--- 403-frog-jump/frog-jump.cpp
+++ 403-frog-jump/frog-jump.cpp
@@ -4,15 +4,11 @@ public:
 
     bool dfs(int jump, int idx, vector<int>& stones,
              unordered_map<int,int>& pos) {
-
         if (idx == stones.size() - 1)
             return true;
-
         if (memo[idx][jump] != -1)
             return memo[idx][jump];
-
         bool ans = false;
-
         // jump - 1
         if (jump - 1 > 0) {
             int nextPos = stones[idx] + jump - 1;
@@ -30,12 +26,11 @@ public:
             if (pos.count(nextPos)) {
                 int nextIdx = pos[nextPos];
                 ans |= dfs(jump, nextIdx, stones, pos);
-            }
+           }
         }
-
         // jump + 1
         {
-            int nextPos = stones[idx] + jump + 1;
+         int nextPos = stones[idx] + jump + 1;
 
             if (pos.count(nextPos)) {
                 int nextIdx = pos[nextPos];
@@ -48,11 +43,9 @@ public:
 
     bool canCross(vector<int>& stones) {
         int n = stones.size();
-
-        unordered_map<int,int> pos;
+ unordered_map<int,int> pos;
         for (int i = 0; i < n; i++)
             pos[stones[i]] = i;
-
         memo.assign(n, vector<int>(n + 1, -1));
 
         if (stones[1] != 1)
