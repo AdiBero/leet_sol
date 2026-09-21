6eab6176a29d92fca3f29dccc2a34b7b1c510c6e
Aditya Singh <adigamer635m@gmail.com>
2026-09-21 20:09:02 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (46.15%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     
-    int maxx = INT_MIN;
     int rec(int i,vector<int>& nums,vector<int>&dp){
         if (i >= nums.size()) 
         return 0;
@@ -9,9 +8,7 @@ public:
     return dp[i];
         int pick = rec(i + 2,nums,dp) + nums[i];
         int skip = rec(i + 1,nums,dp);
-        dp[i] = max(pick,skip);
-
-        return max(pick,skip);
+        return dp[i] = max(pick, skip);
     }
     int rob(vector<int>& nums) {
         vector<int>dp(nums.size(),-1);
