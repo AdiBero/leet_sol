863d1a5cc7afc4c6c5e14802f53689c310712a9d
Aditya Singh <adigamer635m@gmail.com>
2026-09-22 15:26:42 +0530

    Time: 0 ms (100.00%) | Memory: 16.2 MB (35.67%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,39 +1,36 @@
 class Solution {
 public:
+    // state 0 = holding
+    // state 1 = can buy
 
-// state 0 means hold, state 1 mean can buy(no cooloff);
-    int rec(int i, vector<int>& prices, int state,vector<vector<int>>&dp) {
-    if (i >= prices.size())
-        return 0;
+    int rec(int i, vector<int>& prices, int state,
+            vector<vector<int>>& dp) {
 
-    if (state == 1) {
-        // Can buy
-        if(dp[i][1] != -1){
-            return dp[i][1];
+        if (i >= prices.size())
+            return 0;
+
+        if (dp[i][state] != -1)
+            return dp[i][state];
+
+        if (state == 1) {
+            int buy = rec(i + 1, prices, 0, dp) - prices[i];
+            int skip = rec(i + 1, prices, 1, dp);
+
+            return dp[i][state] = max(buy, skip);
         }
-        int buy = rec(i + 1, prices, 0,dp) - prices[i];
-        int skip = rec(i + 1, prices, 1,dp);
+        else {
+            int sell = rec(i + 2, prices, 1, dp) + prices[i];
+            int hold = rec(i + 1, prices, 0, dp);
 
-        return dp[i][1] = max(buy, skip);
-    }
-    else {
-        // Currently holding
-
-        if(dp[i][0] != -1){
-            return dp[i][0];
+            return dp[i][state] = max(sell, hold);
         }
-        int sell = rec(i + 2, prices, 1,dp) + prices[i];
-        int hold = rec(i + 1, prices, 0,dp);
-
-        return dp[i][0] = max(sell, hold);
     }
-}
+
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> dp(n,vector<int>(2,-1));
 
-        return rec(0,prices,1,dp);
+        vector<vector<int>> dp(n, vector<int>(2, -1));
 
-        
+        return rec(0, prices, 1, dp);
     }
 };
