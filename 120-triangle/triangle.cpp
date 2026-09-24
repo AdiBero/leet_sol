c56e50e811622f2117fe1b996598ffd336b91cb2
Aditya Singh <adigamer635m@gmail.com>
2026-09-24 15:09:00 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (24.44%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
-    int dfs(int row, int col,vector<vector<int>>& tri, vector<vector<int>>& dp){
-        if(row == tri.size() - 1){
-            return tri[row][col];
-        }
-        if(dp[row][col] != INT_MAX){
-            return dp[row][col];
-        }
-
-        int leftp = dfs(row + 1, col,tri,dp);
-        int rightp = dfs(row + 1, col + 1,tri,dp);
-
-        return dp[row][col] = tri[row][col] + min(leftp,rightp);
-    }
+    
     int minimumTotal(vector<vector<int>>& triangle) {
         int m = triangle.size();
-        vector<vector<int>> dp(m);
+          vector<vector<int>> dp(m);
 
 for (int i = 0; i < m; i++) {
     dp[i] = vector<int>(triangle[i].size(), INT_MAX);
 }
-        return dfs(0,0,triangle,dp);
         
+
+        for(int i = m-1; i >= 0; i--){
+            for(int j = 0; j < triangle[i].size();j++){
+                if(i == m-1){
+                    dp[i][j] = triangle[i][j];
+                }
+                else{
+                int left = dp[i+1][j];
+                int right = dp[i+1][j+1];
+
+                dp[i][j] = triangle[i][j] + min(left,right);
+                }
+            }
+        }
+        return dp[0][0];
+ 
     }
 };
